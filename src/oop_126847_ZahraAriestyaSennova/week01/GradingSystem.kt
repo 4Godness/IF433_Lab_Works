19688f9601b5e6a67fe47705563a23ce58a61dfd
Ariestya <zahra.sennova@gmail.com>
2026-02-04 16:38:13 +0700

    week01: initial setup and procedural code

--- src/oop_126847_ZahraAriestyaSennova/week01/GradingSystem.kt
+++ src/oop_126847_ZahraAriestyaSennova/week01/GradingSystem.kt
@@ -1,17 +1,8 @@
 package oop_126847_ZahraAriestyaSennova.week01
 
 fun main() {
-    val name ="Zahra Ariestya"
-    val score = 80
+    var name: String ="Zahra Ariestya"
+    var score: Int = 80
 
-    println("Nama: $name, Nilai: $score")
-    println("Status: ${calculateStatus(score)}")
-
-    val studentId: String? = null
-
-    val idLength = studentId?.length ?: 0
-
-    println("Panjang ID: $idLength")
+    println("Nama: "+ name + ", Nilai:" + score)
 }
-
-fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
