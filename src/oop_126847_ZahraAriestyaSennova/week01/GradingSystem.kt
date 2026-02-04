a791d9a2dc2e9dd40b70a060cb0aad54a9f1bbb4
Ariestya <zahra.sennova@gmail.com>
2026-02-04 16:25:09 +0700

    week01: add calculateStatus function

--- src/oop_126847_ZahraAriestyaSennova/week01/GradingSystem.kt
+++ src/oop_126847_ZahraAriestyaSennova/week01/GradingSystem.kt
@@ -5,13 +5,7 @@ fun main() {
     val score = 80
 
     println("Nama: $name, Nilai: $score")
-
-    val grade = when(score){
-        in 90 < .. < 100 -> "A"
-        in 80 < .. < 89 -> "B"
-        in 70 < .. < 79 -> "C"
-        else -> "D"
-    }
-
-    println("Grade: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
