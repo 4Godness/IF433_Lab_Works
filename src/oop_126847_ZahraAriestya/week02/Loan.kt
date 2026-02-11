bfe66eaa7e37aaf0144b59bbb43888798a6a9760
Ariestya <zahra.sennova@gmail.com>
2026-02-11 15:59:35 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_126847_ZahraAriestya/week02/Loan.kt
@@ -0,0 +1,17 @@
+package oop_126847_ZahraAriestya.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    var loanDuration: Int = 1   // Default 1 hari
+) {
+
+    // Durasi Loan
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
