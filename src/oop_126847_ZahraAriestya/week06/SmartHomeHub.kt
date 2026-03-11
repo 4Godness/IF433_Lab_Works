7fb371222cef84acf4f1de767d74207e59873994
Ariestya <zahra.sennova@gmail.com>
2026-03-11 16:01:12 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_126847_ZahraAriestya/week06/SmartHomeHub.kt
+++ src/oop_126847_ZahraAriestya/week06/SmartHomeHub.kt
@@ -8,6 +8,7 @@ class SmartHomeHub {
     }
 
     fun turnOffAllSwitches() {
+        println("=== Mematikan semua perangkat switchable ===")
         for (device in devices) {
             if (device is Switchable) {
                 device.turnOff()
@@ -16,10 +17,20 @@ class SmartHomeHub {
     }
 
     fun activateSecurityMode() {
+        println("=== AKTIVASI MODUS KEAMANAN ===")
         for (device in devices) {
             when (device) {
-                is Recordable -> device.startRecord()
-                is SmartSpeaker -> device.playMusic("Sirine Peringatan")
+                is Recordable -> {
+                    println("  [Security] Mendeteksi device Recordable:")
+                    device.startRecord()
+                }
+                is SmartSpeaker -> {
+                    println("  [Security] Mendeteksi SmartSpeaker:")
+                    device.playMusic("Sirine Peringatan - WARNING!")
+                }
+                else -> {
+                    println("  [Security] ${device.name} tidak merespon mode keamanan")
+                }
             }
         }
     }
