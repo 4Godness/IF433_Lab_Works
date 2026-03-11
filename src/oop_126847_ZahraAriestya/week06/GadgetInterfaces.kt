f0b3b33227fe842762c33fceacf882384d902ceb
Ariestya <zahra.sennova@gmail.com>
2026-03-11 15:28:18 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_126847_ZahraAriestya/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_126847_ZahraAriestya.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.")}
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.")}
+}
