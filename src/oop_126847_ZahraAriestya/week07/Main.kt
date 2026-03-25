f1fc03ab1204580fed308a2d9bec5391f78727fc
Ariestya <zahra.sennova@gmail.com>
2026-03-25 15:26:20 +0700

    week07: implement data class for structural equality

--- src/oop_126847_ZahraAriestya/week07/Main.kt
+++ src/oop_126847_ZahraAriestya/week07/Main.kt
@@ -6,12 +6,18 @@ fun main() {
     DatabaseManager.connect()
 
     println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient() //Instalitation lewat Factory
+    val client = NetworkClient.createClient()
     client.connect()
 
     println("\n=== TEST REGULAR CLASS ===")
     val reg1 = RegularUser("Alice", 22)
     val reg2 = RegularUser("Alice", 22)
-    println(reg1) // Akan mencetak nomor hash
+    println(reg1)
     println("Sama? ${reg1 == reg2}")
+
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 }
--- src/oop_126847_ZahraAriestya/week07/UserModels.kt
+++ src/oop_126847_ZahraAriestya/week07/UserModels.kt
@@ -1,4 +1,5 @@
 package oop_126847_ZahraAriestya.week07
 
-class RegularUser(val name: String, val age: Int) {
-}
+class RegularUser(val name: String, val age: Int)
+
+data class DataUser(val name: String, val age: Int)
