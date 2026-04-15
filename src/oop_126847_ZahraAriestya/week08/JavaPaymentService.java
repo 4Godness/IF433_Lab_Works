affd2ee62752be5b04b02e8bbad9b1b04fc94be7
Ariestya <zahra.sennova@gmail.com>
2026-04-15 15:54:08 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_126847_ZahraAriestya/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_126847_ZahraAriestya.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
