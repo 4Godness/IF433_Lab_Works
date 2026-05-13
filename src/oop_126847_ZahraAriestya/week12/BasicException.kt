faf97261495394d56ff3de35a81d1840164ffacf
Ariestya <zahra.sennova@gmail.com>
2026-05-13 09:03:23 +0700

    week12: create basic try-catch-finally

---
+++ src/oop_126847_ZahraAriestya/week12/BasicException.kt
@@ -0,0 +1,12 @@
+package oop_126847_ZahraAriestya.week12
+
+fun divide(a: Int, b: Int): Int {
+    try {
+        return a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Devision attempt finished")
+    }
+}
